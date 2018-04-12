de945b74d0d9853e26d32ed4b6716057e90d1ccd
Martinez <0142219@students.dadeschools.net>
2018-04-12 11:59:56 -0400

    started some of everything

---
+++ logic.js
@@ -0,0 +1,35 @@
+var panel = $ ("")
+var contStartNumber = 30;
+
+//Question set
+var questions = [{
+	question: "What is the last level of Pac-Man?",
+	answer:["256", "254", "240", "245"],
+	correctAnswer: "256"
+	image: "assets/Splitscreen.gif"
+
+}, {
+	question: "What is chasing Mappy in the game Mappy?",
+	answer:["Cats", "Dogs", "Lizards", "Birds"],
+	correctAnswer: "Cats"
+	image: "assets/download.jpg"
+}, {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
